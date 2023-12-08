7009242e7ecb4623ab519a5cbda2e7666ac4fedc
Konstantin Murugov <konstantin.murugov@rimac-technology.com>
2023-12-08 08:51:29 +0100

    2023-12-08 1 2!

--- 2023/08/1.py
+++ 2023/08/1.py
@@ -7,17 +7,24 @@ input()
 network = {}
 
 for line in sys.stdin:
-    key = line[3]
+    #print(line)
+    key = line[:3]
     left = line[7:10]
     right = line[12:15]
     network[key] = { 'L': left, 'R': right }
+#print(network)
     
 i = 0
+j = 0
 node = 'AAA'
 while True:
     node = network[node][commands[i]]
+    #print(node)
     i += 1
+    j += 1
     if node == 'ZZZ':
         break
+    if i >= len(commands):
+        i = 0
         
-print(i)
+print(j)
