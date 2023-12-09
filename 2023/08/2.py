6b52c8d2c9929fba68b24ca56cd39d0e5d7090a7
Konstantin Murugov <konstantin.murugov@rimac-technology.com>
2023-12-09 12:50:16 +0100

    2023-12-08 2 2

--- 2023/08/2.py
+++ 2023/08/2.py
@@ -1,38 +1,70 @@
 import sys
 
-commands = input()
+commands = []
+line = input()
+for ch in line:
+    if ch == 'L':
+        commands.append(0)
+    else:
+        commands.append(1)
+        
+print(len(commands))
 
 input()
 
-network = {}
+network = []
 nodes = []
+node = 0
+dic = {}
+
+def GetNode(st):
+    global node
+    if st in dic:
+        return dic[st]
+    else:
+        dic[st] = node
+        node += 1
+        return node - 1
 
 for line in sys.stdin:
     #print(line)
-    key = line[:3]
-    left = line[7:10]
-    right = line[12:15]
-    network[key] = { 'L': left, 'R': right }
+    key = GetNode(line[:3])
+    left = GetNode(line[7:10])
+    right = GetNode(line[12:15])
+    if key >= len(network):
+        network += [(0, 0, False)] * (key - len(network) + 1)
+    network[key] = (left, right, line[2] != 'Z')
     
-    if key[2] == 'A':
+    if line[2] == 'A':
         nodes.append(key)
-#print(network)
+print(dic)
+print(network)
+print(nodes)
     
 i = 0
 j = 0
+jj = 0
 while True:
     end = True
+    cmd = commands[i]
     for k in range(len(nodes)):
-        node = network[nodes[k]][commands[i]]
-        nodes[k] = node
-        if node[2] != 'Z':
+        net = network[nodes[k]]
+        if net[2]:
             end = False
-    #print(node)
+        nodes[k] = net[cmd]
+    #print(nodes)
+    if end:
+        break
     i += 1
     j += 1
-    if End:
-        break
     if i >= len(commands):
         i = 0
+        jj += 1
+        if jj > 1000:
+            jj = 0
+            print(j)
+    #print(nodes)
         
 print(j)
+
+# > 100000000
