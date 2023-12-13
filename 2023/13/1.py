be567e5e55daa0db7391ef627bc7361b89e102fe
Konstantin Murugov <konstantin.murugov@rimac-technology.com>
2023-12-13 13:12:28 +0100

    2023-12-13 1 done

--- 2023/13/1.py
+++ 2023/13/1.py
@@ -4,38 +4,44 @@ summ = 0
 
 rows = []
 for line in sys.stdin:
-    if line == '':
+    if len(line) < 3:
         H = len(rows)
         W = len(rows[0])
+        print(H, W)
 
         for i in range(H - 1):
             eq = True
-            for j in range(min(i + 1, H - i)):
+            for j in range(min(i + 1, H - 1 - i)):
                 for k in range(W):
-                    if rows[i - j, k] != rows[i + 1 + j, k]:
+                    #print(i, j, k)
+                    if rows[i - j][k] != rows[i + 1 + j][k]:
                         eq = False
                         break
                 if not eq:
                     break
             if eq:
-                summ + 100 * (i + 1)
+                summ += 100 * (i + 1)
                 break
                 
         for i in range(W - 1):
             eq = True
-            for j in range(min(i + 1, W - i)):
+            for j in range(min(i + 1, W - 1 - i)):
                 for k in range(H):
-                    if rows[k, i - j] != rows[k, i + 1 + j]:
+                    if rows[k][i - j] != rows[k][i + 1 + j]:
+                        #print(i, j, k)
                         eq = False
                         break
                 if not eq:
                     break
             if eq:
-                summ + (i + 1)
+                summ += (i + 1)
                 break
                 
         rows = []
     else:
-        rows.append(line)
+        rows.append(line[:-1])
         
 print(summ)
+
+# > 27159
+# > 27259
