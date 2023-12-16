f6e0079ef29e5e68b180b9804906a5f919159e85
Konstantin Murugov <konstantin.murugov@rimac-technology.com>
2023-12-16 15:58:51 +0100

    2023-12-16 1 done

--- 2023/16/1.py
+++ 2023/16/1.py
@@ -16,7 +16,10 @@ left = (0, -1)
 
 path = []
 for i in range(H):
-    path.append([{up: False, down: False, left: False, right: False}] * W)
+    row = []
+    for j in range(W):
+        row.append({up: False, down: False, left: False, right: False})
+    path.append(row)
     
 rules = {
 '.': {up: [up], down: [down], right: [right], left: [left]},
@@ -28,21 +31,38 @@ rules = {
     
     
 def go(y, x, dr):
-    if not y in range(H) or not x in range(W):
-        return
-    nxt = rules[grid[y][x]][dr]
-    for nx in nxt:
-        if not path[y][x][nx]:
-            path[y][x][nx] = True
-            go(y + nx[0], x + nx[1], nx)
+    while True:
+        if not y in range(H) or not x in range(W):
+            return
+        nxt = rules[grid[y][x]][dr]
+        if len(nxt) > 1:
+            for nx in nxt:
+                if not path[y][x][nx]:
+                    path[y][x][nx] = True
+        #            print(y, x, '->', y + nx[0], x + nx[1])
+                    go(y + nx[0], x + nx[1], nx)
+            return
+        else:
+            if not path[y][x][nxt[0]]:
+                path[y][x][nxt[0]] = True
+                y += nxt[0][0]
+                x += nxt[0][1]
+                dr = nxt[0]
+            else:
+                return
  
 go(0, 0, (0, 1))
 
 summ = 0
 for row in path:
+    s = ''
     for cell in row:
         if cell[up] or cell[down] or cell[left] or cell[right]:
             summ += 1
+            s += '#'
+        else:
+            s += '.'
+    print(s)
             
 print(summ)
 
