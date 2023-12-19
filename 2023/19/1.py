a0a88d7ecaf097c909ea48502e578bc936293d58
Konstantin Murugov <konstantin.murugov@rimac-technology.com>
2023-12-19 10:58:58 +0100

    2023-12-19 1 done

--- 2023/19/1.py
+++ 2023/19/1.py
@@ -16,29 +16,36 @@ for line in sys.stdin:
         if ps > 0:
             wf = line[:ps]
             rules = []
-            for rule in line[ps + 1, len(line) - 1].split(','):
+            for rule in line[ps + 1:-2].split(','):
                 ps = rule.find(':')
                 if ps == -1:
-                    rules.append((lambda rates: rates[0] > -1, rule))
+                    rules.append((0, '>', -1, rule))
                 else:
-                    if rule[1] == '<':
-                        rules.append((lambda rates: rates[categs[rule[0]]] < int(rule[2:]), rule[ps + 1:]))
-                    else:
-                        rules.append((lambda rates: rates[categs[rule[0]]] > int(rule[2:]), rule[ps + 1:]))
+                    rules.append((categs[rule[0]],
+                                rule[1],
+                                int(rule[2:ps]),
+                                rule[ps + 1:]
+                    ))
             workflows[wf] = rules
         else:
-            rates = line[1:-1].split(',')
+            rates = line[1:-2].split(',')
             part = [0] * 4
             for rate in rates:
                 part[categs[rate[0]]] = int(rate[2:])
+            print(part)
             wf = 'in'
             while not wf in ['A', 'R']:
                 rules = workflows[wf]
                 for rule in rules:
-                    if rule[0](part):
-                        wf = rule[1]
+                    if (rule[1] == '<' and part[rule[0]] < rule[2]) or (rule[1] == '>' and part[rule[0]] > rule[2]):
+                        wf = rule[3]
+                        print(wf)
                         break
             if wf == 'A':
+                print(part[0])
                 summ += sum(part)
                 
+print(workflows['rfg'])
+                
 print(summ)
+# < 791714
