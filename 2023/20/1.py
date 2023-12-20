8b173bc16ed9f983f84279b332f9b77adb0a224f
Konstantin Murugov <konstantin.murugov@rimac-technology.com>
2023-12-20 12:06:23 +0100

    2023-12-20 1 1

--- 2023/20/1.py
+++ 2023/20/1.py
@@ -16,7 +16,7 @@ class Button(Module):
         for rcv in self.rcvs:
             Pulses.append((self, rcv, False))
             
-class Broadcaster(Module)
+class Broadcaster(Module):
     def Process(self, trnsmtr, pulse):
         for r in self.rcvs:
             Pulses.append((self, r, pulse))
@@ -49,24 +49,30 @@ for line in sys.stdin:
             modules[name] = Flipflop(name)
         elif line[0] == '&':
             modules[name] = Conjunction(name)
-    lines[name] = line[line.find('->') + 3:].split(', ')
+    lines[name] = line[line.find('->') + 3:-1].split(', ')
 modules['button'].AddRcv(modules['broadcaster'])    
 for name in lines:
     for r in lines[name]:
         trn = modules[name]
-        rcv = modules[r]
+        if r in modules:
+            rcv = modules[r]
+        else:
+            rcv = None
         trn.AddRcv(rcv)
         if type(rcv) is Conjunction:
             rcv.AddTransmitter(trn)
             
 for i in range(1000):
     modules['button'].Push()
-    while len(pulses) > 0:
-        pulse = pulses.pop(0)
+    while len(Pulses) > 0:
+        pulse = Pulses.pop(0)
         if pulse[2]:
             HighCnt += 1
         else:
             LowCnt += 1
-        pulse[1].Process(pulse[0], pulse[2])
+        if pulse[1]:
+            pulse[1].Process(pulse[0], pulse[2])
 
-print(LowCnt * HighCnt)        
+print(LowCnt * HighCnt)     
+
+# > 615127392   
