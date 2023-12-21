17db3c055f260552840d5ca1dc18b30f4008e8db
Konstantin Murugov <konstantin.murugov@rimac-technology.com>
2023-12-21 11:10:31 +0100

    2023-12-21 1 done

--- 2023/21/1.py
+++ 2023/21/1.py
@@ -13,16 +13,16 @@ H = len(garden)
 W = len(garden[0])    
 print(H, W)
 
-nxtPlots = [(y, x)]
+nxtPlots = {(y, x)}
 
 def paint(y, x):
     if y in range(H) and x in range(W) and garden[y][x] != '#':
-        nxtPlots.append((y, x))
+        nxtPlots.add((y, x))
 
 for steps in range(64):
     print(steps, len(nxtPlots))
     plots = nxtPlots
-    nxtPlots = []
+    nxtPlots = set()
     for plot in plots:
         paint(plot[0] - 1, plot[1])
         paint(plot[0] + 1, plot[1])
