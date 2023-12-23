e141ef15da4055697cacc98857d3175d6e8de7df
Konstantin Murugov <konstantin.murugov@rimac-technology.com>
2023-12-23 12:49:46 +0100

    2023-12-23 1 1

--- 2023/23/1.py
+++ 2023/23/1.py
@@ -13,26 +13,26 @@ print(H, W)
 longest = 0
 nxt = []
 
-def Process(x, y):
+def Process(x, y, path):
     if x in range(W) and y in range(H) and not (y, x) in path and lines[y][x] != '#':
         nxt.append((y, x))
 
 def Recurse(pos, path):
     while True:
-        if pos == (H - 1, W - 2)
+        if pos == (H - 1, W - 2):
             longest = max(longest, len(path))
             return
         path.add(pos)
         nxt = []
         ch = lines[pos[0]][pos[1]]
-        if ch in ['.', '^']
-            Process(pos[0] - 1, pos[1])
-        if ch in ['.', '>']
-            Process(pos[0], pos[1] + 1)
-        if ch in ['.', 'v']
-            Process(pos[0] + 1, pos[1])
-        if ch in ['.', '^']
-            Process(pos[0], pos[1] - 1)
+        if ch in ['.', '^']:
+            Process(pos[0] - 1, pos[1], path)
+        if ch in ['.', '>']:
+            Process(pos[0], pos[1] + 1, path)
+        if ch in ['.', 'v']:
+            Process(pos[0] + 1, pos[1], path)
+        if ch in ['.', '^']:
+            Process(pos[0], pos[1] - 1, path)
         if len(nxt) == 1:
             pos = nxt[0]
         else:
