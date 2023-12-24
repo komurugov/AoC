b41f85c5a6df2f57059b1f0a71660f04e2be7a5c
Konstantin Murugov <konstantin.murugov@rimac-technology.com>
2023-12-24 18:43:48 +0100

    2023-12-24 1 done

--- 2023/24/1.py
+++ 2023/24/1.py
@@ -10,24 +10,26 @@ for line in sys.stdin:
         print('oops')
     a = vy / vx
     b = y1 - a * x1
-    paths.append([a, b])
+    paths.append((a, b, x1, vx))
     
 P = len(paths)    
 cnt = 0
-low = 7 #200000000000000
-high = 27 #400000000000000
+low = 200000000000000
+high = 400000000000000
     
 for p1 in range(P - 1):
+    pp1 = paths[p1]
     print(p1)
     for p2 in range(p1 + 1, P):
+        pp2 = paths[p2]
         #print(':', p2, paths[p1], paths[p2])
         div = paths[p1][0] - paths[p2][0]
         if div != 0:
             x = (paths[p2][1] - paths[p1][1]) / div
-            if x >= low and x <= high:
+            if x >= low and x <= high and (pp1[3] > 0 and x > pp1[2] or pp1[3] < 0 and x < pp1[2]) and (pp2[3] > 0 and x > pp2[2] or pp2[3] < 0 and x < pp2[2]):
                 y = paths[p1][0] * x + paths[p1][1]
                 if y >= low and y <= high:
-                    print(p1, p2, x, y)
+                    #print(p1, p2, x, y)
                     cnt += 1
                     
 print(cnt)
