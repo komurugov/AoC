c771853974048b568055f5eba38f41bd54c3aa34
Konstantin Murugov <konstantin.murugov@rimac-technology.com>
2023-12-24 18:33:08 +0100

    2023-12-24 1 1

--- 2023/24/1.py
+++ 2023/24/1.py
@@ -4,8 +4,8 @@ paths = []
 
 for line in sys.stdin:
     line = line.split('@')
-    x1, y1, z1 = line[0].split(',')
-    vx, vy, vz = line[1].split(',')
+    x1, y1, z1 = list(map(int, line[0].split(',')))
+    vx, vy, vz = list(map(int, line[1].split(',')))
     if vx == 0:
         print('oops')
     a = vy / vx
@@ -14,16 +14,22 @@ for line in sys.stdin:
     
 P = len(paths)    
 cnt = 0
-limits = range(2E14, 4E14 + 1)
+low = 7 #200000000000000
+high = 27 #400000000000000
     
 for p1 in range(P - 1):
+    print(p1)
     for p2 in range(p1 + 1, P):
+        #print(':', p2, paths[p1], paths[p2])
         div = paths[p1][0] - paths[p2][0]
         if div != 0:
             x = (paths[p2][1] - paths[p1][1]) / div
-            if x in limits:
+            if x >= low and x <= high:
                 y = paths[p1][0] * x + paths[p1][1]
-                if y in limits:
+                if y >= low and y <= high:
+                    print(p1, p2, x, y)
                     cnt += 1
                     
 print(cnt)
+
+# < 30256
